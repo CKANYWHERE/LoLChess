6ac73685352c85399761376f6021a9b6cf7faf2a
Min-Chang-Kyeoung <janu723@naver.com>
2020-06-10 23:39:38 +0900

    주석제거

--- app/src/main/java/com/lolchess/strategy/view/adapter/ChampMainAdapter.kt
+++ app/src/main/java/com/lolchess/strategy/view/adapter/ChampMainAdapter.kt
@@ -18,13 +18,7 @@ import java.lang.RuntimeException
 class ChampMainAdapter(private val context : Context, private val items: List<Champ>)// recycler view binding 해주는 클래스
     : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    /*override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChampMainViewHolder
-    {
-        val view = LayoutInflater.from(context).inflate(R.layout.champ_home_item, parent, false)
-        Log.e("CreateChampViewHolder","onCreate")
-        return ChampMainViewHolder(view)
 
-    }*/
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):  RecyclerView.ViewHolder
     {
 
@@ -45,41 +39,7 @@ class ChampMainAdapter(private val context : Context, private val items: List<Ch
 
     }
     override fun getItemCount(): Int  = items.size
-
-
-    /*override fun onBindViewHolder(holder: ChampMainViewHolder, position: Int) {
-        Log.e("BindViewHolder","BindView")
-
-        var synergySize = items[position]?.synergy.size
-        var cost = items[position]?.cost
-
-        when(cost){
-            1 -> { holder.imgCost.setBackgroundColor(Color.rgb(173,181,189)) }
-            2 -> { holder.imgCost.setBackgroundColor(Color.rgb(81,207,102)) }
-            3 -> { holder.imgCost.setBackgroundColor(Color.rgb(76,110,245)) }
-            4 -> { holder.imgCost.setBackgroundColor(Color.rgb(204,93,232)) }
-            5 -> { holder.imgCost.setBackgroundColor(Color.rgb(255,255,0)) }
-        }
-
-        holder.imgMain.setImageResource(items[position]?.imgPath)
-        holder.txtMain.text = items[position]?.name
-        if(synergySize == 2) { //시너지가 2개일 경우
-            holder.firstImgSyn.setImageResource(items[position]?.synergy[0]?.imgPath)
-            holder.secondImgSyn.setImageResource(items[position]?.synergy[1]?.imgPath)
-            holder.txtFirstSyn.text = items[position]?.synergy[0].name
-            holder.txtSecondSyn.text = items[position]?.synergy[1].name
-        }
-        else{ // 시너지가 3개일 경우
-            holder.firstImgSyn.setImageResource(items[position]?.synergy[0]?.imgPath)
-            holder.secondImgSyn.setImageResource(items[position]?.synergy[1]?.imgPath)
-            holder.thirdImgSyn.setImageResource(items[position]?.synergy[2]?.imgPath)
-            holder.txtFirstSyn.text = items[position]?.synergy[0].name
-            holder.txtSecondSyn.text = items[position]?.synergy[1].name
-            holder.txtThirdSyn.text = items[position]?.synergy[2].name
-        }
-
-
-    }*/
+    
 
     override fun getItemViewType(position: Int): Int = items[position].type
 
