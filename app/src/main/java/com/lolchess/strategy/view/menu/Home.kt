c6c0f4070bcc3b39f3e50de45019c85aeda54f47
Min-Chang-Kyeoung <janu723@naver.com>
2020-07-14 17:53:57 +0900

    가로 화면 고정 이슈 수정

--- app/src/main/java/com/lolchess/strategy/view/menu/Home.kt
+++ app/src/main/java/com/lolchess/strategy/view/menu/Home.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.ads.MobileAds
+import com.google.android.youtube.player.YouTubePlayerView
 import com.lolchess.strategy.R
 import com.lolchess.strategy.model.data.RecommandMetaData
 import com.lolchess.strategy.view.adapter.RecommendMetaAdapter
@@ -21,18 +22,19 @@ class Home:Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.home_fragment, container, false)
+        //val youTubePlayerView:YouTubePlayerView = view.findViewById(R.id.you_tube_player_view)
 
-        //activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        //activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
-
-
         val recommendData = RecommandMetaData().getAllMetaData()
         val recommandAdapter = RecommendMetaAdapter(recommendData, view.context)
 
         recommendMetaView?.layoutManager = LinearLayoutManager(view.context)
         recommendMetaView?.adapter = recommandAdapter
+
+
     }
 }
